c5f173a439712436b8d4bd53debd785dfde16390
Odile <41570723+Odile-nza@users.noreply.github.com>
2021-07-18 19:36:26 +0200

    Assignment 1 @Traffic light

---
+++ Traffic_light.X/source.c
@@ -0,0 +1,43 @@
+/*
+ * File:   source.c
+ * Author: odile
+ *
+ * Created on July 16, 2021, 8:24 AM
+ */
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define  _XTAL_FREQ 8000000
+void main(void) {
+    TRISB=0b00000000;
+    PORTB=0X00;
+    while(1)
+    {
+        PORTBbits.RB0=1;
+        __delay_ms(500);
+         PORTBbits.RB0=0;
+        __delay_ms(500);
+        
+         PORTBbits.RB1=1;
+        __delay_ms(400);
+         PORTBbits.RB1=0;
+        __delay_ms(400);
+        
+         PORTBbits.RB2=1;
+        __delay_ms(300);
+         PORTBbits.RB2=0;
+        __delay_ms(300);
+         
+    }
+ 
+}
